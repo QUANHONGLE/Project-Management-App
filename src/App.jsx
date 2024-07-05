8931a7c885d2ac1e9fdef8443dfb3cbfca422a91
Quan Le <quanle@Quans-MBP-2.attlocal.net>
2024-07-04 21:45:30 -0500

    Add project buttons will now open new project component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,37 @@
 import NewProject from "./components/NewProject";
+import NoProjectSelected from "./components/NoProjectSelected";
 import Sidebar from "./components/Sidebar";
 
+import { useState } from "react";
+
 function App() {
+  const [projectState, setProjectState] = useState({
+    selectedProjectID: undefined,
+    projects: [],
+  });
+
+  function handleStartAddProject() {
+    setProjectState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectID: null,
+      };
+    });
+  }
+
+  let content;
+
+  if (projectState.selectedProjectID === null) {
+    content = <NewProject />;
+  } else if (projectState.selectedProjectID === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <Sidebar />
-      <NewProject />
+      <Sidebar onStartAddProject={handleStartAddProject} />
+      {/* <NewProject /> */}
+      {content}
     </main>
   );
 }
